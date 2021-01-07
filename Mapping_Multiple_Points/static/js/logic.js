1cf0abfd4b4d62a222f52b35ff1bd719820d4b20
Jose-Monagas <josemonagas212@gmail.com>
2021-01-06 21:19:27 -0500

    updating Map_line files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -8,14 +8,6 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 let cityData = cities;
 console.log(cities);
 
-// Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location)
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//     .addTo(map);
-// });
-
 // Loop through the cities array and create one marker (circle) for each city.
 cityData.forEach(function(city) {
     console.log(city)
